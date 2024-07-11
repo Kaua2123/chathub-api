6a6050b59eb1a88d4c3a2baaf9d1213cf1061e2b
Kaua2123 <kauantnetsoares@gmail.com>
2024-07-11 10:04:35 -0300

    build: altera tsconfig.json

--- src/modules/users/dto/create-user-dto.ts
+++ src/modules/users/dto/create-user-dto.ts
@@ -1,4 +1,5 @@
 import { Length } from 'class-validator';
+import { Unique } from 'src/custom-decorators/Unique';
 
 export class CreateUserDto {
   id: number;
@@ -7,14 +8,14 @@ export class CreateUserDto {
   name: string;
 
   @Length(5, 20)
-  // @Unique({
-  //   message: 'Username is already in use.',
-  // })
+  @Unique({
+    message: 'Username is already in use.',
+  })
   username: string;
 
-  // @Unique({
-  //   message: 'Email is already in use.',
-  // })
+  @Unique({
+    message: 'Email is already in use.',
+  })
   email: string;
 
   password: string;
